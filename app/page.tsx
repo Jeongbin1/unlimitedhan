7a4c62ae7d41f8b4d978aac9cba9a3af4f99f254
Jeongbin1 <petitjb05@gmail.com>
2025-04-16 17:13:35 +0900

    style: 공통 스타일 추가 (font, favicon)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen ">
+    <div className="flex flex-col items-center justify-between min-h-screen ">
       HOME
-    </main>
+    </div>
   );
 }
